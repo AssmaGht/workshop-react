96fcf646c7ec5639157af3747348ec63ac8cea87
AssmaGht asma <assmaghattassi@gmail.com>
2024-10-27 23:40:11 +0000

    Initial commit

---
+++ server/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true, // Allows requests from any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
+  const config =new DocumentBuilder()
+   .setTitle('workshop')
+   .setDescription('The workshop API')
+   .setVersion('1.0')
+   .addTag('workshop nestjs')
+   .build();
+
+   const documentFactory = () =>  SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('api', app, documentFactory);
+   await app.listen(process.env.PORT ?? 3000);
+
+}
+bootstrap();
